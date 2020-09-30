870d489f7fe743566b8ab5ffcbfaf1a5413ed989
Chris DeSilva <chris.a.desilva@gmail.com>
2020-09-30 06:45:20 -0500

    Update logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     siteLanguage: `en`,
     logo: `src/images/skillsFund_logo.png`,
     favicon: `src/images/Favicon-SF.png`,
-    keywords: `coding loans, bootcamp loans, tuition loan, tuition financing, coding, web development, UX design, UI design coding bootcamp, bootcamp, full-time, part-time, student financing, student loans, loans, cosigner, underwriting, credit, credit score`
+    keywords: `coding loans, bootcamp loans, tuition loan, tuition financing, coding, web development, UX design, UI design coding bootcamp, bootcamp, full-time, part-time, student financing, student loans, loans, cosigner, underwriting, credit, credit score`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,9 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-netlify`,
     {
-      resolve: 'gatsby-plugin-zopfli'
+      resolve: "gatsby-plugin-zopfli",
     },
     // {
     //   resolve: `gatsby-plugin-google-gtag`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,8 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage, createRedirect } = actions
+  createRedirect({
+    fromPath: "/go",
+    toPath: "/",
+  })
+  createRedirect({ fromPath: "/accepted", toPath: "/" })
+}
